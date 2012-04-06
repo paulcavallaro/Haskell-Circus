42469b0994ee7d0cb09adcc87c2c2e730a7ceea3
Paul Cavallaro <paulcavallaro@gmail.com>
2012-04-06 00:08:20 -0400

    Adding the pathological example from Matt Might's compilers page

---
+++ pathological.py
@@ -0,0 +1,25 @@
+# Comment 1
+ # Comment 2
+
+# Factorial:
+
+def fact(  x\
+):
+
+  if x == -1:
+    return 1.j
+
+  elif x ==0:
+
+    return 1
+  else:
+
+        return x* fact(x
+
+- 1)
+
+s = "foo\
+\\ \n\'\""
+
+
+fact(20)
