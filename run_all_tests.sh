d476300f515d506a9815392476c4b2e0a3f66428
Paul Cavallaro <paulcavallaro@gmail.com>
2012-04-09 20:59:44 -0400

    Adding pathological to the test suite

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,6 +1,6 @@
 echo "Testing pylex..."
 
-for input in simple_def.py string_literals.py byte_literals.py integer_literals.py float_literals.py imaginary_literals.py indent.py
+for input in simple_def.py string_literals.py byte_literals.py integer_literals.py float_literals.py imaginary_literals.py indent.py pathological.py
 do
     echo 
     echo "---------------------------------------------------------------"
